3002ac0525cb4d59f19f82946d8e16ade2a4ac96
vishnu <vappu5429@gmail.com>
2022-12-08 23:44:31 +0530

    initial commit

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,40 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+export default function ManualHeader() {
+
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } = useMoralis()
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                // enableWeb3()
+            }
+        }
+        console.log("hiiiiii")
+        console.log(isWeb3Enabled)
+    },/*if we give nothing in here it goes for circular rendering*/[isWeb3Enabled])
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("NUll found")
+            }
+
+        })
+    })
+
+    return (
+        <div>
+            {account ? (<div>Connected to {account.slice(0, 6)}...{account.length - 4} </div>) : <button onClick={async () => {
+                await enableWeb3()
+                window.localStorage.setItem("connected", "inject")
+
+            }} disabled={isWeb3EnableLoading}>
+                Connect</button>}
+        </div>)
+
+}
